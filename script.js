26472a9d084666a640e3ec83d71aac1bbda983ec
anticN <anticn@bzz.ch>
2022-10-06 16:21:15 +0200

    feat: add delete

--- script.js
+++ script.js
@@ -6,12 +6,37 @@ const createCell = (cellText) => {
     return cell;
 }
 
+const createButton = (id) => {
+    const buttId = id
+    const button = document.createElement("button")
+    button.innerText = "Delete"
+    button.className = "delButton"
+    return button
+}
+
+function deleteTask(id){
+    fetch(`http://localhost:3000/task/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+    location.reload();
+}
+
 function renderTasks(tasks){
+    //const delButton = document.getElementsByClassName("delButton");
     const tableBody = document.querySelector("tbody")
     tasks.forEach((task) => {
         const tableRow = document.createElement("tr") 
-        tableRow.append(createCell(task.id), createCell(task.title), createCell(task.completed))
+        tableRow.append(createCell(task.id), createCell(task.title), createCell(task.completed));
+        const delButton = createButton(task.id)
+        tableRow.append(delButton)
         tableBody.appendChild(tableRow)
+        delButton.addEventListener("click", () => {
+            deleteTask(task.id)
+            indexTasks()
+        })
     })
 }
 
@@ -26,13 +51,12 @@ function indexTasks() {
 
 
 
+
 document.addEventListener("DOMContentLoaded", () => {
     const allTasks = document.getElementById("allTasks");
     const newTask = document.getElementById("newTask");
     const updateTask = document.getElementById("updateTask");
-    const delTask = document.getElementById("delTask"); 
-    const newTaskForm = document.getElementById("newTaskForm")
-    const newTaskInput = document.getElementById("newTaskInput"); 
+     
     
 
     allTasks.addEventListener("click", () => {
@@ -42,9 +66,4 @@ document.addEventListener("DOMContentLoaded", () => {
     updateTask.addEventListener("click", () => {
         alert("Tes3")
     })
-
-    delTask.addEventListener("click", () => {
-        alert("Test4")
-    })
-        
 })
